43c5d19d251021232eed5eaf3f1ceacedfec1909
rachelharu <rachelharu9@gmail.com>
2021-08-31 12:43:01 -0700

    added audio and animations when button is clicked. also set playSound and animate Press to userChosenColour in click(function).

--- game.js
+++ game.js
@@ -5,18 +5,19 @@ var userClickedPattern = [];
 
 var randomNumber;
 
-
 function nextSequence() {
   randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
 
-};
+  //animations and sounds
+  $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeIn(100);
+  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  audio.play();
+}
 
 
-// $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeIn(100);
-// var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
-// audio.play();
+
 
 
 $( ".btn" ).click(function() {
@@ -25,4 +26,23 @@ var userChosenColour = $(this).attr("id");
 //alert(userChosenColour);
 userClickedPattern.push(userChosenColour);
 console.log(userClickedPattern);
+playSound(userChosenColour);
+animatePress(userChosenColour);
 });
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+
+function animatePress(currentColor) {
+
+
+$("#" + currentColor).addClass("pressed");
+
+  setTimeout(function() {
+  $("#" + currentColor).removeClass("pressed");
+  },  100);
+
+}
