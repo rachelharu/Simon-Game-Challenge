313b23c721e2b5ff8340095635368517bc28b27f
rachelharu <rachelharu9@gmail.com>
2021-09-01 12:02:21 -0700

    added function to check answers to give boolean value, works.

--- game.js
+++ game.js
@@ -1,55 +1,65 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
-var level = [0];
-
+var level = 0;
+var started = false;
 // var randomNumber;
 
-$(document).keypress(function(e) {
-    if(e.which == 97) {
-         alert('You pressed a!');
+//starts game
+$(document).keypress(function() {
+  if (!started) {
+    level++;
+    $("#level-title").text("level " + level);
+    nextSequence();
+    started = true;
+  }
+});
 
-
-
-
+//starts sequence
 function nextSequence() {
- var randomNumber = Math.floor(Math.random() * 4);
+  level++;
+  $("#level-title").text("level " + level);
+  var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
 
   //animations and sounds
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
   playSound(randomChosenColour);
-  level++;
-
 }
 
-}
-});
-
-
-
-
+//checks users click and sends lights and animations to corresponding button
 $(".btn").click(function() {
   var userChosenColour = $(this).attr("id");
   //alert(userChosenColour);
   userClickedPattern.push(userChosenColour);
-  console.log(userClickedPattern);
+  // console.log(userClickedPattern);
   playSound(userChosenColour);
   animatePress(userChosenColour);
+
 });
 
+//function to check user answers
+function checkAnswer(currentLevel){
+// if (userClickedPattern == gamePattern)
+//  {
+//    console.log("yes");
+//  } else {
+//    console.log("poop");
+//  }
+return JSON.stringify(userClickedPattern)==JSON.stringify(gamePattern);
+}
+
+
+// function for sounds
 function playSound(name) {
   var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
 }
 
-
+//function for animation
 function animatePress(currentColor) {
-
-
   $("#" + currentColor).addClass("pressed");
-
   setTimeout(function() {
     $("#" + currentColor).removeClass("pressed");
   }, 100);
